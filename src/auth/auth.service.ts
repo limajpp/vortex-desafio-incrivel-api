b57fc4584661409eee00856330069c540db8fa23
João Pedro Lima <joaopedrocal90@gmail.com>
2025-11-25 16:50:30 -0300

    feat: added password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from 'src/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,15 +10,18 @@ export class AuthService {
     @InjectRepository(User) private readonly repo: Repository<User>,
   ) {}
   async createUser(name: string, email: string, password: string) {
-    const doesGivenEmailAlreadyExists = (await this.repo.findOneBy({ email }))
+    const doesGivenEmailAlreadyExist = (await this.repo.findOneBy({ email }))
       ? true
       : false;
-    if (doesGivenEmailAlreadyExists)
+    if (doesGivenEmailAlreadyExist)
       throw new BadRequestException(
         `The e-mail provided is already registered`,
       );
 
-    const newUser = this.repo.create({ name, email, password });
+    const SALTS = 10;
+    const hash = await bcrypt.hash(password, SALTS);
+    const newUser = this.repo.create({ name, email, password: hash });
+
     return await this.repo.save(newUser);
   }
 }
