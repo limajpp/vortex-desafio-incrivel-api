5d50ca43ffe425c5d93848048dcba58386302cc2
João Pedro Lima <joaopedrocal90@gmail.com>
2025-11-26 15:47:35 -0300

    feat: finished 'r' from crud

--- src/expenses/expense.controller.ts
+++ src/expenses/expense.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { CreateExpenseDTO } from './DTOs/create-expense-dto';
 import { ExpenseService } from './expense.service';
@@ -18,5 +26,16 @@ export class ExpenseController {
     );
   }
 
-  
+  @Get()
+  getAllExpenses(
+    @Request() req,
+    @Query('month') month?: string,
+    @Query('year') year?: string,
+  ) {
+    const userId = req.user.sub;
+    const monthNumber = month ? Number(month) : undefined;
+    const yearNumber = year ? Number(year) : undefined;
+
+    return this.serv.listExpenses(userId, monthNumber, yearNumber);
+  }
 }
--- src/expenses/expense.service.ts
+++ src/expenses/expense.service.ts
@@ -9,7 +9,12 @@ export class ExpenseService {
     @InjectRepository(Expense) private readonly repo: Repository<Expense>,
   ) {}
 
-  async addExpense(description: string, amount: number, date: Date, userId: number) {
+  async addExpense(
+    description: string,
+    amount: number,
+    date: Date,
+    userId: number,
+  ) {
     if (!description || !amount || !date)
       throw new BadRequestException(
         `Something is missing... Please insert the description, amount and date of the expense to proceed.`,
@@ -19,5 +24,21 @@ export class ExpenseService {
 
     const newExpense = this.repo.create({ description, amount, date, userId });
     return await this.repo.save(newExpense);
+  }
+
+  async listExpenses(userId: number, month?: number, year?: number) {
+    const query = this.repo
+      .createQueryBuilder('expense')
+      .where('expense.userId = :userId', { userId });
+
+    if (month) {
+      query.andWhere('EXTRACT(MONTH FROM expense.date) = :month', { month });
+    }
+    if (year) {
+      query.andWhere('EXTRACT(YEAR FROM expense.date) = :year', { year });
+    }
+    query.orderBy('expense.date', 'DESC');
+
+    return await query.getMany();
   }
 }
